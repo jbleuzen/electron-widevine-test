ddb3b5c3f410e143d2cdc1434a8c54634597ff24
Johan Bleuzen <jbleuzen@molotov.tv>
2016-05-16 15:45:36 +0200

    Update for electron 1.1.0

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 //let widevine_adapter_path= app.getPath('appData').concat('/electron-quick-start/widevine/1.4.8.866/_platform_specific/mac_x64/widevinecdmadapter.plugin');
 
-let widevineVersion = '1.4.8.866';
-let baseWidevinePath ='./accentué/'+widevineVersion+'/_platform_specific/';
+let widevineVersion = '1.4.8.885';
+let baseWidevinePath ='./Widevine/'+widevineVersion+'/_platform_specific/';
 var widevine_adapter_path ='';
 
 switch(process.platform)
